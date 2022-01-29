75cc773db6f81fb8e3ff8186ddaafd04810c35c8
Dragon Robotics <dragonrobotics2375@gmail.com>
2022-01-29 11:12:57 -0700

    1. Finished intake command

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,11 +5,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeCommand extends CommandBase {
   /** Creates a new IntakeCommand. */
-  public IntakeCommand() {
+
+  private final IntakeSubsystem m_intake;
+
+  public IntakeCommand(
+    IntakeSubsystem intake
+  ) {
+    m_intake = intake;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +26,15 @@ public class IntakeCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_intake.intake();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_intake.stopMotor();
+  }
 
   // Returns true when the command should end.
   @Override
