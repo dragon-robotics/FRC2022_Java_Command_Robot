783cf2c5de88fcbca948b6e3a24c467c8b6dc2af
Doug Liu <dougdliu@gmail.com>
2022-03-18 00:48:56 -0700

    1. Completed all the 2 ball autos

--- src/main/java/frc/robot/commands/Auto/TwoBallBotLeftLowGoalCommand.java
+++ src/main/java/frc/robot/commands/Auto/TwoBallBotLeftLowGoalCommand.java
@@ -8,26 +8,37 @@ import java.util.List;
 
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.GenerateTrajectory;
 import frc.robot.AutoLoader.AutoCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.UptakeSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class TwoBallBotLeftLowGoalCommand extends ParallelCommandGroup {
+public class TwoBallBotLeftLowGoalCommand extends SequentialCommandGroup {
   /** Creates a new TwoBallBotLeftLowGoalCommand. */
 
   private Trajectory p1, p2; // Part 1 and 2 of the trajectory
 
   // This command will require:
   // - Drivetrain subsystem
-  // - Shooter subsystem
   // - Intake subsystem
+  // - Uptake subsystem
+  // - Shooter subsystem
   public TwoBallBotLeftLowGoalCommand(
-      DrivetrainSubsystem drivetrain,
-      AutoCommand command) {
+    DrivetrainSubsystem drivetrain,
+    IntakeSubsystem intake,
+    UptakeSubsystem uptake,
+    ShooterSubsystem shooter,
+    AutoCommand command
+  ) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
 
@@ -38,16 +49,24 @@ public class TwoBallBotLeftLowGoalCommand extends ParallelCommandGroup {
     p2 = trajectories.get(1);
 
     /**
-     * Command Sequence all ran in parallel:
-     * 1. Engage Shooter and Intake - <Parallel>
-     * 2. new Sequential Command
-     * a. Move forward
-     * b. Shoot ball to low goal for x seconds
-     * c. Move backwards
+     * Command Sequence in sequential order:
+     * 1. Intake cargo while moving backward
+     * 2. Move forward while still intaking cargo for 2 more seconds
+     * 3. Shoot ball for 2 seconds
      */
     addCommands(
-        new SequentialCommandGroup(
+        new ParallelDeadlineGroup(
             GenerateTrajectory.getRamseteCommand(p1, drivetrain),
-            GenerateTrajectory.getRamseteCommand(p2, drivetrain)));
+            new IntakeCargoAutoCommand(intake, uptake)),
+        new ParallelCommandGroup(
+            GenerateTrajectory.getRamseteCommand(p2, drivetrain),
+            new ParallelRaceGroup(
+                // Intake the ball for 2 more seconds to make sure it's really in there
+                new WaitCommand(2),
+                new IntakeCargoAutoCommand(intake, uptake))),
+        new ParallelRaceGroup(
+            // Shoot the ball for 2 seconds
+            new WaitCommand(2),
+            new ShootAutoCommand(uptake, shooter)));
   }
 }
