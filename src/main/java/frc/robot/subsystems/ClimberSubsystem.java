d9d8061769db3b40fb9825fe1b2e7cc9e010e5db
Dragon Robotics <dragonrobotics2375@gmail.com>
2022-03-19 00:20:18 -0700

    1. Added the climber subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,12 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new Climber. */
-  public ClimberSubsystem() {}
 
+  CANSparkMax m_climberL = new CANSparkMax(9, MotorType.kBrushless);
+  CANSparkMax m_climberR = new CANSparkMax(10, MotorType.kBrushless);
+
+  public ClimberSubsystem() {
+    // Invert 1 side of the climber //
+    m_climberL.setInverted(true);
+  }
+
+  public void climb(double speed) {
+    m_climberL.set(speed);
+    m_climberR.set(speed);
+  }
+
+  public void stopMotor(){
+    m_climberL.set(0);
+    m_climberR.set(0);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
